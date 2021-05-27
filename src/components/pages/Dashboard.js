2cf887bd1391bff8e212e653d14483f53a7eb151
Bera <65181065+berakoc@users.noreply.github.com>
2021-05-27 19:47:10 +0300

    *New: Modulized Dashboard

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import FirebaseAuthAPI from '../../api/firebase-auth';
+import combine from '../../lib/style-composer';
+import { logoutSession } from '../../redux/actions/session';
 import { User } from '../components';
+import styles from '../../styles/pages/Dashboard.module.scss';
 
-function Dashboard() {
+const mapDispatchToProps = (dispatch) => ({
+    logout: () => dispatch(logoutSession()),
+});
+
+function Dashboard({ logout }) {
+    const handleLogout = async () => {
+        FirebaseAuthAPI.logout();
+        await logout();
+        console.log('Successfully logged out');
+    };
     return (
-        <User isSpecial />
+        <div className={combine(styles, 'component')}>
+            <User isSpecial />
+            <div className={combine(styles, 'navbar')}><button onClick={() => handleLogout()}>Logout</button></div>
+        </div>
     )
 }
 
-export default Dashboard
+export default connect(null, mapDispatchToProps)(Dashboard)
