5b97cfaa53d71a47c2ee811324ad6a617c3d79e9
Bera <65181065+berakoc@users.noreply.github.com>
2021-06-05 17:12:43 +0300

    New: Added style for event filter

--- src/components/atoms/Filter.js
+++ src/components/atoms/Filter.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
+import styles from '../../styles/atoms/Filter.module.scss'
+import combine from '../../lib/style-composer';
 
 function Filter(props) {
     return (
-        <div>
-            <div>{props.title}</div>
+        <div className={combine(styles, 'component')}>
+            <div className={combine(styles, 'title')}>{props.title}</div>
             <div>
                 {props.names.map((name, index) => (
-                    <span key={nanoid()} onClick={props.filters[index]}>
+                    <span className={combine(styles, 'item', !index ? 'active' : ':null')} key={nanoid()} onClick={props.filters[index]}>
                         {name}
                     </span>
                 ))}
